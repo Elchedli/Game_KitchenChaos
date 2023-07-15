d4715957412d6715f1582a7994ac5a06e8c99c05
Elchedli <shidonosan@gmail.com>
2023-07-15 19:51:28 +0100

    progress : 7:06:20 Delivery manager

--- KitchenChaos/Assets/Scripts/DeliveryCounter.cs
+++ KitchenChaos/Assets/Scripts/DeliveryCounter.cs
@@ -6,8 +6,10 @@ public class DeliveryCounter : BaseCounter{
 
     public override void Interact(Player player) {
         if (player.HasKitchenObject()) {
-            if(player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
-            player.GetKitchenObject().DestroySelf();
+            if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject)) {
+                //only accepts plates
+                player.GetKitchenObject().DestroySelf();
+            }
         }
     }
 }
