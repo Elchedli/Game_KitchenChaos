1119b1ea2905b70c186a8ba716f202bc447eddb2
Elchedli <shidonosan@gmail.com>
2023-07-19 21:01:41 +0100

    progress : 7:21:34 Delivery Manager UI

--- KitchenChaos/Assets/Scripts/DeliveryCounter.cs
+++ KitchenChaos/Assets/Scripts/DeliveryCounter.cs
@@ -2,16 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeliveryCounter : BaseCounter
-{
+public class DeliveryCounter : BaseCounter {
 
-    public override void Interact(Player player)
-    {
-        if (player.HasKitchenObject())
-        {
-            if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
-            {
+    public override void Interact(Player player) {
+        if (player.HasKitchenObject()) {
+            if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject)) {
                 //only accepts plates
+                DeliveryManager.Instance.DeliverRecipe(plateKitchenObject);
                 player.GetKitchenObject().DestroySelf();
             }
         }
